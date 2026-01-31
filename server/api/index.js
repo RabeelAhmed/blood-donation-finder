31208672f6678351442dd493e2861693cdbc616d
RabeelAhmed <rabeelsulehria3@gmail.com>
2026-01-31 22:46:15 +0500

    Fix API URL for production

--- server/api/index.js
+++ server/api/index.js
@@ -34,6 +34,9 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+module.exports = app;
